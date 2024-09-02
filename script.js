56b7f07fc07529babd1848813aeb432a8911041f
dxse <dm739165@gmail.com>
2024-09-02 13:06:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const btnElement = document.getElementById("button");
+const birthdayElement = document.getElementById("birthday");
+const resultElement = document.getElementById("result");
+
+function calculateUserAge() {
+  const birthdayValue = birthdayElement.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultElement.innerText = `Your age is ${age} ${
+      age > 1 ? "years" : "year"
+    } old`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btnElement.addEventListener("click", calculateAge);
